1e2340a6fd3a7fc527c324b850bb1a0d5651a46d
dell <jchhura21@gmail.com>
2022-09-15 18:41:44 +0530

    done

--- app/src/main/java/com/example/webtonativeapp/MainActivity.kt
+++ app/src/main/java/com/example/webtonativeapp/MainActivity.kt
@@ -9,6 +9,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private var onBackPressed=0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
